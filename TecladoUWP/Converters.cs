bbe7fb41c4e6d538a1cef266c4b269192c6d8bfd
Cris Carmona <cris_her_95@hotmail.com>
2016-09-10 13:20:24 -0500

    Creacion de repositorio

---
+++ TecladoUWP/Converters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+
+namespace TecladoUWP
+{
+    public class BoolToSolidBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string str)
+        {
+            if ((bool)value)
+            {
+                return new SolidColorBrush(Windows.UI.Color.FromArgb(128, 128, 128, 128));
+            }
+            else
+            {
+                return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 128, 128, 128));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string str)
+        {
+            return null;
+        }
+    }
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string str)
+        {
+            switch (parameter as string)
+            {
+                case "VisibleIfTrue": return (bool)value == true ? Visibility.Visible : Visibility.Collapsed;
+                case "CollapsedIfTrue": return (bool)value == true ? Visibility.Collapsed : Visibility.Visible;
+                default: return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string str)
+        {
+            return null;
+        }
+    }
+}
---
+++ TecladoUWP/OnScreenKeyboard.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TecladoUWP
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public partial class OnScreenKeyBoard : UserControl
+    {
+        #region Properties
+        public object Host { get; set; }
+
+        public KeyboardLayouts InitialLayout
+        {
+            get { return (this.DataContext as KeyboardViewModel).Layout; }
+            set { (this.DataContext as KeyboardViewModel).Layout = value; }
+        }
+        public static readonly DependencyProperty InitialLayoutProperty = DependencyProperty.Register("InitialLayout", typeof(KeyboardLayouts), typeof(OnScreenKeyBoard), new PropertyMetadata(KeyboardLayouts.English));
+        #endregion
+
+        public OnScreenKeyBoard()
+        {
+            DataContext = new KeyboardViewModel(this);
+            InitializeComponent();
+        }
+
+        public void RegisterTarget(TextBox control)
+        {
+            RegisterBox(control);
+        }
+        public void RegisterTarget(PasswordBox control)
+        {
+            RegisterBox(control);
+        }
+        private void RegisterBox(Control control)
+        {
+            control.GotFocus += delegate { (DataContext as KeyboardViewModel).TargetBox = control; System.Diagnostics.Debug.WriteLine("focused"); };
+            control.LostFocus += delegate { (DataContext as KeyboardViewModel).TargetBox = null; };
+        }
+    }
+}
